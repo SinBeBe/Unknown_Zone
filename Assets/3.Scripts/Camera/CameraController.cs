887ded338e817a41ddbb7d3f394b965bc0a32912
SinBe <kimtjwnsyba@gmail.com>
2024-10-24 09:31:32 +0900

    player bug fix

--- Assets/3.Scripts/Camera/CameraController.cs
+++ Assets/3.Scripts/Camera/CameraController.cs
@@ -4,6 +4,10 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
+    private Transform playerPos;
+    private Vector3 cameraPos;
+
+    [SerializeField]
     private float mouseSensitivity = 100f;
     private float xRotation = 0;
 
@@ -11,6 +15,8 @@ public class CameraController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        cameraPos = playerPos.position + new Vector3(0f, 3f, 0f);
     }
 
     private void Update()
@@ -18,15 +24,21 @@ public class CameraController : MonoBehaviour
         RotateCamera();
     }
 
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(playerPos.position.x, cameraPos.y, playerPos.position.z);
+    }
+
     private void RotateCamera()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * (mouseSensitivity * Time.deltaTime);
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
-        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerPos.Rotate(Vector3.up * mouseX);
     }
 }
