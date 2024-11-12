4cfb7e0ba0f2edd0db182fffc529668e131378db
SinBe <kimtjwnsyba@gmail.com>
2024-11-13 08:15:16 +0900

    Update

--- Assets/3.Scripts/Item/Syringe.cs
+++ Assets/3.Scripts/Item/Syringe.cs
@@ -5,7 +5,7 @@ public class Syringe : ItemBase
 {
     public override IEnumerator ItemUsed()
     {
-        //플레이어 속도 증가, 데미지 절반 감소
+        //gi.Player.playerData.WalkSpeed =
         yield return new WaitForSeconds(15f);
         gi.isUsedItem = false;
     }
--- Assets/3.Scripts/Manager/GameManager.cs
+++ Assets/3.Scripts/Manager/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
-    public GameObject Player;
+    public PlayerController Player;
 
     [SerializeField]
     private List<GameObject> candleLight = new List<GameObject>();
