bba27d2f7b18979b0ab1ae65b8c06666da0df960
SinBe <kimtjwnsyba@gmail.com>
2024-11-29 17:07:55 +0900

    Update Fake.cs

--- Assets/3.Scripts/Ghost/GhostSkill/Fake.cs
+++ Assets/3.Scripts/Ghost/GhostSkill/Fake.cs
@@ -9,22 +9,26 @@ public class Fake : GhostSkillBase
     [SerializeField]
     private Transform centerPos;
 
-    private Vector3[] generationPos;
+    private Transform[] generationPos;
 
 
     void Start()
     {
         int rand = Random.Range(1, 6);
-        generationPos = new Vector3[rand];
+        generationPos = new Transform[rand];
 
         for (int i = 0; i < rand; i++)
         {
-            generationPos[i] = GeneratePosition();
+            generationPos[i].position = GeneratePosition(transform.position, 80f);
+            Instantiate(fakeGhostPre, generationPos[i]);
         }
     }
 
-    private Vector3 GeneratePosition()
+    private Vector3 GeneratePosition(Vector3 pos, float range)
     {
-        return centerPos.position;
+        float x = Random.Range(pos.x - range, pos.x + range);
+        float z = Random.Range(pos.z - range, pos.z + range);
+
+        return new Vector3(x, 5f, z);
     }
 }
