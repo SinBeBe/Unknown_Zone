97bda7ad219cefe2630e05b13b0c130fa1807606
SinBe <kimtjwnsyba@gmail.com>
2025-05-21 10:19:25 +0900

    Edit AudioMixer

--- Assets/3.Scripts/Manager/GameManager.cs
+++ Assets/3.Scripts/Manager/GameManager.cs
@@ -53,7 +53,6 @@ public class GameManager : MonoBehaviour
         if(instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(this.gameObject);
         }
         else
         {
